b63f5fa2562dd6978607551657d0b76d86a861f3
AbdulWasay25 <awasay2500@gmail.com>
2024-09-13 15:47:13 +0500

    Add files via upload

---
+++ LAB TASK 4/WASAY 2.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("ENTER ANY NUMBER\n");
+	scanf("%d",&n);
+	switch(n){
+		case 1:
+			printf("ONE");
+			break;
+		case 2:
+		    printf("TWO");
+			break;
+			case3:
+			printf("THREE");
+			break;
+		case 4:
+		printf("FOUR");
+		break;
+		case 5:
+		printf("FIVE");
+		break;	
+		case 6:
+		printf("SIX");
+		break;	
+			case 7:
+		printf("SEVEN");
+		break;	
+			case 8:
+		printf("EIGHT");
+		break;	
+			case 9:
+		printf("NINE");
+		break;	
+			case 10:
+		printf("TEN");
+		break;	
+			case 11:
+		printf("ELEVEN");
+		break;	
+			case 12:
+		printf("TWELVE");
+		break;	
+			case 13:
+		printf("THIRTEEN");
+		break;	
+			case 14:
+		printf("FOURTEEN");
+		break;	
+			case 15:
+		printf("FIFTEEN");
+		break;	
+			case 16:
+		printf("SIXTEEN");
+		break;	
+			case 17:
+		printf("SEVENTEEN");
+		break;	
+			case 18:
+		printf("EIGHTEEN");
+		break;	
+			case 19:
+		printf("NINETEEN");
+		break;	
+			case 20:
+		printf("TWENTY");
+		break;
+		default :
+		printf("NUMBER GREATER THAN 20");		
+	}
+	
+	return 0;
+}
