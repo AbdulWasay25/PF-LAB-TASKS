fdd17997122df57b19f8251c5737888db5612f25
AbdulWasay25 <awasay2500@gmail.com>
2024-09-14 00:04:57 +0500

    Add files via upload

---
+++ PF LAB 4/Untitled4.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	printf("\t\t  ____________________\n");
+	printf("\t\t \b / \t\t      \\ \n");
+    printf("\t\t \b/     /\\_/\\ \t       \\ \n");
+    printf("\t       \b /     ( . . )\t   ^    \\ \n");
+    printf("\t      \b /       )   ( \t  ((     \\ \n");
+    printf("\t     \b |       ( > < )_)) \t  | \n");
+    printf("\t      \b \\       ----- \t         / \n");
+    printf("\t       \b \\      k24-0744    \t/  \n");
+    printf("\t\t \b\\           \t       /  \n");
+    printf("\t\t \b \\ \t\t      /  \n");
+    printf("\t\t  ____________________\n");
+}
